32517163a5f2a96ed132b10400737798f1253d66
Cristian Ceballos <ceballosmarincristiancamilo@gmail.com>
2026-04-28 14:49:54 -0500

    Se sube primer punto del segundo ejercicio de biblioteca mendiante array HashSet

--- src/biblioteca/Biblioteca.java
+++ src/biblioteca/Biblioteca.java
@@ -1,4 +1,23 @@
 package biblioteca;
+import java.util.HashSet;
 
 public class Biblioteca {
+
+    public static void main(String[] args) {
+
+        //Creal lista tipo HashSet
+        HashSet<String> biblioteca = new HashSet<>();
+
+        //agregart elementos
+        biblioteca.add("Un Grito Desesperado");
+        biblioteca.add("Volar Sobre el pantano");
+        biblioteca.add("Hábitos Atómicos");
+        biblioteca.add("Sangre de Campeón");
+
+        // Mostrar biblioteca
+        System.out.println(" ***************** Bienvenido a la bilioteca de Cris *****************");
+        for (String libro : biblioteca) {
+            System.out.println("- " + libro);
+        }
+    }
 }
